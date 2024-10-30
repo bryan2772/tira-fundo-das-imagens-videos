982a636ec71c2885d41bc7c986e0452ec10a33b5
bryan2772 <ryanpassosmg@gmail.com>
2024-10-29 22:26:37 -0300

    removeusando e imprime passo a passo rembg

--- codigo/removefundotodas.py
+++ codigo/removefundotodas.py
@@ -1,6 +1,7 @@
 from rembg import remove
 from PIL import Image
 import os
+import matplotlib.pyplot as plt
 
 # Diretórios de entrada e saída
 input_dir = 'ImagensPDI/'  # Diretório das imagens de entrada
@@ -11,18 +12,31 @@ os.makedirs(output_dir, exist_ok=True)
 
 # Itera sobre todos os arquivos do diretório de entrada
 for filename in os.listdir(input_dir):
-    # Verifica se o arquivo é uma imagem
     if filename.endswith(('.png', '.jpg', '.jpeg')):
-        # Caminho completo da imagem de entrada e de saída
         input_path = os.path.join(input_dir, filename)
         output_path = os.path.join(output_dir, filename)
 
-        # Carrega a imagem e remove o fundo
-        imagem = Image.open(input_path)
+        # Carrega a imagem e converte para escala de cinza
+        imagem = Image.open(input_path).convert("L")
+        plt.imshow(imagem, cmap='gray')
+        plt.title(f"Imagem em Cinza: {filename}")
+        plt.axis('off')
+        plt.show()
+
+        # Remove o fundo
         imagemremovida = remove(imagem)
+        plt.imshow(imagemremovida)
+        plt.title(f"Imagem sem Fundo: {filename}")
+        plt.axis('off')
+        plt.show()
 
-        # Converte para RGB antes de salvar como JPEG
-        imagemremovida = imagemremovida.convert("RGB")
-        imagemremovida.save(output_path)
+        # Converte para RGB e exibe colorida
+        imagemremovida_rgb = imagemremovida.convert("RGB")
+        plt.imshow(imagemremovida_rgb)
+        plt.title(f"Imagem Colorida Final: {filename}")
+        plt.axis('off')
+        plt.show()
 
+        # Salva a imagem final sem fundo em RGB
+        imagemremovida_rgb.save(output_path)
         print(f'Imagem {filename} processada e salva em {output_path}')
